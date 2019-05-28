4b66ac521f81710c641166621767791733cf3869
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-28 16:12:56 -0700

    Created AirbenderList component and added static data for styling

---
+++ src/components/AirbenderList.js
@@ -0,0 +1,24 @@
+import Component from './Component.js';
+
+class AirbenderList extends Component {
+    renderTemplate() {
+        return /*html*/`
+            <ul id="airbender-list">
+                <li>
+                    <h1>46th Earth King</h1>
+                    <img src="https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441">
+                </li>
+                <li>
+                    <h1>46th Earth King</h1>
+                    <img src="https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441">
+                </li>
+                <li>
+                    <h1>46th Earth King</h1>
+                    <img src="https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441">
+                </li>
+            </ul>
+        `;
+    }
+}
+
+export default AirbenderList;
