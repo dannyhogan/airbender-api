365cf0fb21a05d922fb22eb4a6b4825f57df85cd
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-28 16:15:40 -0700

    Created base AirbenderItem component

---
+++ src/components/AirbenderItem.js
@@ -0,0 +1,14 @@
+import Component from './Component.js';
+
+class AirbenderItem extends Component {
+    renderTemplate() {
+        return /*html*/`
+            <li>
+                <h1>46th Earth King</h1>
+                <img src="https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441">
+            </li>
+        `;
+    }
+}
+
+export default AirbenderItem;
