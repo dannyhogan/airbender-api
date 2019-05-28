6f11e41ca24a4f2804889c1c644d792e7730f5bd
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-28 16:45:27 -0700

    Updated CharacterItem component to display data from api

--- src/components/AirbenderItem.js
+++ src/components/AirbenderItem.js
@@ -2,10 +2,12 @@ import Component from './Component.js';
 
 class AirbenderItem extends Component {
     renderTemplate() {
+        const airbender = this.props.airbender;
+
         return /*html*/`
             <li>
-                <h1>46th Earth King</h1>
-                <img src="https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441">
+                <h1>${airbender.name}</h1>
+                <img src="${airbender.photoUrl}">
             </li>
         `;
     }
