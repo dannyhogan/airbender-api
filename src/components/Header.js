13eaf4d1fa0b257a4a9ef960322a0fcc02b82cb8
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-29 15:38:39 -0700

    Added props to Header component to update label

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,21 @@ import Component from './Component.js';
 
 class Header extends Component {
     renderTemplate() {
+        let character = this.props.search;
+        let type = this.props.searchPrefix;
+
+        if(!type) {
+            type = '';
+        }
+
+        if(!character) {
+            character = 'All Character';
+        }
+
         return /*html*/`
             <header>
                 <h1>The Last Airbender</h1>
-                <h3>Character List</h3>
+                <h3>${character}'s ${type}</h3>
             </header>
         `;
     }
