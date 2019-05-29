c874caab2f53b36da02d6d940a8036be51df63d9
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-29 15:46:00 -0700

    Passed header component props to update label

--- src/components/App.js
+++ src/components/App.js
@@ -10,19 +10,21 @@ class App extends Component {
         const app = this.renderDOM();
         const main = app.querySelector('main');
 
-        const header = new Header();
+        const header = new Header({ search: '', searchPrefix: '' });
         app.insertBefore(header.render(), main);
 
         const airbenderList = new AirbenderList({ airbenders: [] });
         main.appendChild(airbenderList.render());
 
-        const loading = new Loading({ loading: true });
+        const loading = new Loading({ loading: false });
         main.appendChild(loading.render());
 
         function getAirbenders() {
+            loading.update({ loading: true });
+
             const params = window.location.hash.slice(1);
             const searchParams = new URLSearchParams(params);
-
+            
             let searchPrefix = '';
 
             if(searchParams.get('allies')) {
@@ -32,6 +34,7 @@ class App extends Component {
             }
 
             const search = searchParams.get(searchPrefix);
+            header.update({ search, searchPrefix });
 
             airbenderApi.getAirbenders(search, searchPrefix)
                 .then(airbenders => {
